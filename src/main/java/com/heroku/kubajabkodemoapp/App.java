32cce66eb9970259b741a31757e44ce635edb2f1
Jakub Chochol <kubachochol94@gmail.com>
2020-08-08 16:33:48 +0200

    SpringBoot login disabled

--- src/main/java/com/heroku/kubajabkodemoapp/App.java
+++ src/main/java/com/heroku/kubajabkodemoapp/App.java
@@ -2,8 +2,9 @@ package com.heroku.kubajabkodemoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class App {
 
 	public static void main(String[] args) {
@@ -11,3 +12,5 @@ public class App {
 	}
 
 }
+
+
