c9c39c489fdfa5404e82116f23aaec5bcdeee574
Jakub Chochol <kubachochol94@gmail.com>
2020-08-08 10:09:46 +0200

    Few tweaks

--- src/main/java/com/heroku/kubajabkodemoapp/controllers/HelloController.java
+++ src/main/java/com/heroku/kubajabkodemoapp/controllers/HelloController.java
@@ -1,4 +1,4 @@
-package com.heroku.kubajabkodemoapp.demo.controllers;
+package com.heroku.kubajabkodemoapp.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
